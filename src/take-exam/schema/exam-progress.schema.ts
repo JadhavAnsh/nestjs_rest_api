f3d032f401b796d3a53e2db155e9ead194a6a1f0
KRISHNAA9922 <krishna.213249104@vcet.edu.in>
2025-07-03 18:06:01 +0530

    examProgress updated schema code final

--- src/take-exam/schema/exam-progress.schema.ts
+++ src/take-exam/schema/exam-progress.schema.ts
@@ -3,35 +3,37 @@ import mongoose, { Document, Types } from "mongoose";
 
 @Schema({ timestamps: true })
 export class ExamProgress extends Document {
+  @Prop({ required: true })
+  totalQuestions: number;
+
+  @Prop({required:true})
+  correctQuestions: number;
+
+  @Prop({ type: Boolean, default: false })
+  hasStarted: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  isCompleted: boolean;
+
+  @Prop({ type: Number, default: 0 })
+  percentage: number;
+
+  @Prop({ type: Date, default: null })
+  completedAt: Date | null;
+
   @Prop({
     type: {
-      hasStarted: { type: Boolean, default: false },
-      isCompleted: { type: Boolean, default: false },
-      percentage: { type: Number, default: 0 },
-      completedAt: { type: Date, default: null },
-      isLearningMaterialCompleted: { type: Boolean, default: false },
-      assessment: {
-        type: {
-          totalAttempts: { type: Number, default: 0 },
-          lastAttemptedAt: { type: Date, default: null },
-          maxPercentageAchieved: { type: Number, default: 0 },
-          attemptLog: { type: [{ type: mongoose.Schema.Types.Mixed }], default: [] }
-        }
-      }
-    }
+      totalAttempts: { type: Number, default: 0 },
+      lastAttemptedAt: { type: Date, default: null },
+      maxPercentageAchieved: { type: Number, default: 0 },
+      attemptLog: { type: [{ type: mongoose.Schema.Types.Mixed }], default: [] },
+    },
   })
-  progress: {
-    hasStarted: boolean;
-    isCompleted: boolean;
-    percentage: number;
-    completedAt: Date | null;
-    isLearningMaterialCompleted: boolean;
-    assessment: {
-      totalAttempts: number;
-      lastAttemptedAt: Date | null;
-      maxPercentageAchieved: number;
-      attemptLog: any[];
-    };
+  assessment: {
+    totalAttempts: number;
+    lastAttemptedAt: Date | null;
+    maxPercentageAchieved: number;
+    attemptLog: any[];
   };
 }
 
